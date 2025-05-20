44c4bfb077bbfb89f81f0177332ea9e44a54bcc4
sjh4616 <41767000+sjh4616@users.noreply.github.com>
2025-05-20 14:59:11 +0900

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -29,6 +29,3 @@ sudo chmod +x /etc/init.d/codedeploy-start.sh
 sudo usermod -aG docker ubuntu
 sudo systemctl enable docker
 sudo systemctl start docker
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
